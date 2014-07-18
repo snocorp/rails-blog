c589a46bfebe76eb0d19cfc296d7b319e2b89482
Dave Sewell <snocorp@gmail.com>
2014-07-18 14:40:04 -0400

    - added ability to edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,20 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
